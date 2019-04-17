812e7393a6fd04337bd3c03610ec82ce99ecd83b
kaltok84 <kaltok84@gmail.com>
2019-04-18 00:37:21 +0900

    KSHDb

--- SqliteAgent/Form1.cs
+++ SqliteAgent/Form1.cs
@@ -14,9 +14,33 @@ namespace SqliteAgent {
         public Form1() {
             InitializeComponent();
         }
+        /*
+         * 2017/12
+         * mc : 매출액
+         * bp : 영익
+         * bpo :영익 오피셜
+         * bbp : 세전계속사업익
+         * qp : 당기순익
+         * bt : 자산총계
+         * minust : 부채총계
+         * moneyt : 자본총계
+         * money : 자본금
+         * af : 영업활동현금흐름
+         * if : 투자활동흐름
+         * mf : 재무활동흐름
+         * 
+         */        
 
         private void Form1_Load(object sender, EventArgs e) {
-            
+            List<KshDb.ColumnInfo> stockColumnInfo = new List<KshDb.ColumnInfo> {
+                new KshDb.ColumnInfo("date", KshDb.DataType.TEXT, true),
+                new KshDb.ColumnInfo("open", KshDb.DataType.INT),
+                new KshDb.ColumnInfo("high", KshDb.DataType.INT),
+                new KshDb.ColumnInfo("low", KshDb.DataType.INT),
+                new KshDb.ColumnInfo("close", KshDb.DataType.INT)
+            };
+
+
         }
     }
 }
--- SqliteAgent/KshDb.cs
+++ SqliteAgent/KshDb.cs
@@ -8,6 +8,32 @@ using System.IO;
 
 namespace SqliteAgent {
     class KshDb {
+        public enum DataType {
+            INT = 0,
+            TEXT,            
+            FLOAT,
+        };
+
+        public class ColumnInfo {
+            public bool primaryKey = false;
+            public DataType dataType;
+            public string columnName;
+            public ColumnInfo(string columnName, DataType dataType, bool primaryKey = false) {
+                this.columnName = columnName;
+                this.dataType = dataType;
+                this.primaryKey = primaryKey;
+            }
+        }
+
+        public class TableInfo {
+            public string tableName;
+            public List<ColumnInfo> columnList = new List<ColumnInfo>();
+
+            public TableInfo(string tableName) {
+                this.tableName = tableName;
+            }
+        }
+
         private SQLiteConnection conn = null;
 
         public KshDb(string databaseFullPath) {
@@ -19,8 +45,26 @@ namespace SqliteAgent {
             conn.Open();
         }
 
-        public void checkTable(string tableName, string createQuery) {
+        public void createTable(TableInfo tableInfo) {
+            if (tableInfo.tableName == null || tableInfo.tableName.Length == 0 || tableInfo.columnList.Count == 0) {
+                throw new ArgumentException("not enough tableInfo");
+            }
+            string sql = "create table if not exists " + tableInfo.tableName + " (";
+            foreach (ColumnInfo info in tableInfo.columnList) {
+                if (info.primaryKey) {
+                    sql += info.columnName + " " + info.dataType.ToString() + "primary key ,";
+                } else {
+                    sql += info.columnName + " " + info.dataType.ToString() + ",";
+                }
+            }
+            sql = sql.Substring(0, sql.Length - 1);
+            sql += ")";
+            new SQLiteCommand(sql, conn).ExecuteNonQuery();
+        }
 
+        public void createStockTable(string code) {
+            string sql = "create table if not exists " + code + " (date char(8) primary key, open int, high int, low int, close int, vol int)";
+            new SQLiteCommand(sql, conn).ExecuteNonQuery();
         }
 
         public void addStockChartData(string code, int open, int high, int low, int close, long volume) {
